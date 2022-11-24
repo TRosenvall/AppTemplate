2271101a0bedbbe3adf26ffad96a84ac2ae9203b
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2022-11-24 02:15:25 -0700

    Initial Commit

---
+++ AppTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppTemplate
+//
+//  Created by Timothy Rosenvall on 11/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
